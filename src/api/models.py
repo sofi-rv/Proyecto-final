953d1f9865972fbbec0cccf031c94f8585d405e6
Sofía <sofirv2901@gmail.com>
2023-09-06 17:03:40 -0600

    backend-registro-usuario

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    nombre_completo = db.Column(db.String(200), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -15,6 +16,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "nombre_completo": self.nombre_completo,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/register.css";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { store, actions } = useContext(Context);
+  let navigate = useNavigate(""); // useHistory("")
+  const register = async () => {
+    //Sección de verificación
+    if (password == "" || email == "" || name == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "¡Por favor llene todos los campos!",
+        /* footer: '<a href="">Why do I have this issue?</a>' */
+        timer: 3500,
+      });
+      return;
+    }
+    //Sección para enviar la data al backend
+    let obj = {
+      nombre_completo: name,
+      email: email,
+      password: password,
+    };
+    let response = await actions.fetchPromise("/api/register", "POST", obj);
+    if (response.ok) {
+      let responseJson = await response.json();
+      console.log(responseJson);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: responseJson.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      navigate("/"); // history.push("/")
+    } else {
+      let responseJson = await response.json();
+      console.log(responseJson);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "¡Error al registrar!",
+        /* footer: '<a href="">Why do I have this issue?</a>' */
+        timer: 3500,
+      });
+    }
+    return;
+  };
+
   return (
     <div className="register_page">
       <form className="register_form">
@@ -10,16 +61,25 @@ export const Register = () => {
         <input
           type="text"
           placeholder="Nombre completo"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
           className="register_input mb-4"
         />
         <input
           type="email"
           placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           className="register_input mb-4"
         />
         <input
           type="password"
           placeholder="Contraseña"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
           className="register_input mb-4"
         />
         <input
@@ -31,6 +91,7 @@ export const Register = () => {
           type="submit"
           value="Registrarse"
           className="register_button mb-4"
+          onClick={register}
         />
         <Link to="/" className="register_link mb-4">
           ¿Ya tenias una cuenta? Volver a login
